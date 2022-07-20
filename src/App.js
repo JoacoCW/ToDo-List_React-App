4ef0a54026f6e1870fe44165cd394e4e8f2fa591
Joaquín Weiss <joaco.w93@gmail.com>
2022-07-20 19:05:31 -0300

    finished to do list app

--- src/App.js
+++ src/App.js
@@ -19,6 +19,13 @@ function App() {
     setInput("");
   };
 
+  const deleteTodo = (id) => {
+    //Filter out todo with the id
+    const newList = list.filter((todo) => todo.id !== id);
+
+    setList(newList);
+  };
+
   return (
     <div>
       <h1>ToDo List</h1>
@@ -28,6 +35,15 @@ function App() {
         onChange={(e) => setInput(e.target.value)}
       />
       <button onClick={() => addTodo(input)} >Add</button>
+
+      <ul>
+        {list.map((todo) => (
+          <li key={todo.id}>
+            {todo.todo}
+            <button onClick={() => deleteTodo(todo.id)} >&times;</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
